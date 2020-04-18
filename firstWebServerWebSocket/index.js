834b23daeb7c30ba20d507e5487401fc8e4ec351
Your Name <you@example.com>
2020-04-18 22:08:29 +0430

    first init

---
+++ firstWebServerWebSocket/index.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const websocket = require('ws');
+
+const server = http
+	.createServer(function (request, response) {
+		response.writeHead(200, { 'Content-Type': 'text/plain' });
+		response.end('Hello World');
+	})
+	.listen(5000);
+
+const wss = new websocket.Server({ server });
+
+wss.on('headers', (headers, req) => {
+	// when new connection has been triggered
+	console.log('this is headers',headers);
+});
+
+wss.on('connection', (ws, req) => {
+	ws.on('message', (msg) => {
+		console.log('-----', msg, '------');
+    });
+    
+    ws.send('welcome to websocket server');
+});
+
+console.log('Server running at http://localhost:5000/');
